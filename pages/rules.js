f4da05326be77b719ccf69e2712382802b546bb9
Alex Irvine <alex.b.irvine@gmail.com>
2019-08-05 06:45:12 -0400

    Made progress on rules, fixed some style

--- pages/rules.js
+++ pages/rules.js
@@ -70,13 +70,30 @@ let pageHTML = `
 <p>So if a character was using the Treat Wounds perk, they would roll the appropriate skill (in this case First aid).</p>
 <p>If they succeed with a roll of 10, they would have a degree of success at 5.  Therefor they would heal 13 HP.</p>
 
-<h3>1.4 - Roll Modifiers</h3>
+<h3>1.4 - Critical Fail & Success</h3>
+<p>Whenever a 1 or 20 is rolled on die, it is considered a critical fail or success respectively.</p>
+<p>Critical failures result in the skill automatically failing, and additional bad things happening.</p>
+<p>To determine what bad things happen, consult the "critical failure" table in the glossary.</p>
+</br>
+<p>Critical successes always succeed no matter what the goal for the roll was.  Thus a character with no skills has a 5% to succeed at anything.</p>
+<p>Critical successes always have 10 degrees of success.</p>
+
+<h3>1.5 - Roll Modifiers</h3>
 <p>Modifiers are used in situations where the task at hand is either easier or harder to perform due to specific circumstances.</p>
 <p>Making an attack against a restrained enemy, attempting to sneak past a guard while wearing bells, and balancing on a tightrope with heavy winds are all examples of circumstances that change the difficulty of the task.</p>
 <p>The GM can decide to add or subtract to the skill check goal depending on the situation.  These modifiers can be +/- 2, 4, 6, 8, or 10.  Modifiers can also stack.</p>
 <p>Example: A player decides to shoot an arrow at a bear who is unaware of her presence (+4 modifier), but during a light rainstorm (-2 modifier).</p>
 <p>The GM would inform the player that she only receives a +2 bonus after adding all modifiers.</p>
 <p>Modifiers can also be permanently added through perks (such as the Archery Specialization), and are usually recoded next to the skill on the character sheet.</p>
+
+<h3>1.6 - Positive vs Negative Modifiers</h3>
+<p>Positive modifiers not only affect the target goal for a skill, but also the outcome.</p>
+<p>If a player with 10 acrobatics skill rolls an 8, they would pass (rolled 10 or under) with 4 degrees of success.</p>
+<p>If that same player had a +2 modifier, they would pass with 5 degrees of success, since the modifier is included in the roll calculation.</p>
+</br>
+<p>Negative modifiers are not considered when calculating degrees of success.</p>
+<p>If the same player with 10 acrobatics skill has a -2 modifier, they would have to roll an 8 or under (as opposed to the normal 10 or under)</p>
+<p>If they roll an 8, they still get 4 degrees of success.</p>
 
 
   <h2>2 - Character Creation</h2>
@@ -93,7 +110,10 @@ let pageHTML = `
 
   <h3>2.2 - Select Race</h3>
   <p>Your race will determine your size and give you perks only found to that race.</p>
-  <p>There are 19 races in total</p>
+  <p>There are 19 races in total.</p>
+  </br>
+  <p>Robots/Constructs are the exception to character building, as they do not calculate attributes normally.</p>
+  <p>They instead have their attributes built into their body parts that are purchased.  To them heros are made, not born.</p>
 
   <h3>2.3 - Set Attributes</h3>
   <p>Attributes are what determines your AP, MP, HP, and what perks/items/spells you can use.</p>
@@ -111,7 +131,39 @@ let pageHTML = `
   <h3>2.6 - Spend starting GP</h3>
   <p>Each player starts out with 500 GP</p>
 
-  <h2>3 - Turns, using AP, and general combat</h2>
+  <h2>3 - Turns & using AP</h2>
+  
+  <h3>3.1 - Initiative</h3>
+  <p>Initiative in this system is taken at the beginning of each session.  To get your initiative, roll the d20 and have the GM record the score.  Players with higher initiative go first.</p>
+  <p>The first reason to only take initiative at the beginning of the session is to avoid giving away surprising moments.  Nothing puts a player on guard like the GM saying "roll initiative".</p>
+  <p>The second reason is to keep things streamlined and sped up.</p>
+  </br>
+  <p>During sections that don't require AP (or roleplaying sections), initiative is still used.  Players who rolled the highest initiative will be able to act first in each session.</p>
+  
+  <h3>3.2 - AP & Turns</h3>
+  <p>AP (action points) determine how much a character can do in each turn.  Each action costs an amount of AP, and turns end when the player has used all of their AP.</p>
+  <p>AP refreshes with each turn.</p>
+  <p>Some actions might require more AP than a player has left in that turn.  In these situations the action will take place over multiple turns.</p>
+  </br>
+  <p>Example: Say a player with 12 AP wants to run over to a player 4 squares away (4 AP) and perform a Treat Wounds skill (10 AP).   Since the player only has 8 AP after moving, she will spend 8 this turn and 2 from her next turn to complete the action.</p>
+  <p>The action will end on the players next turn (when she spends all the AP required), and she can be interrupted before the action ends.  See the Combat section for more details on skill interruption.</p>
+  
+  <h2>4 - Health, damage, conditions, and death</h2>
+  <h3>4.1 - Health and Damage</h3>
+  <p>Health (or HP) is determined from the players endurance score, and each body section has an individual health score.</p>
+  <p>There are 4 body sections for each character: Head, Torso, Arms, Legs.</p>
+  <p>The body has HP equal to 50 time the character's endurance score.  Arms have 20 times the characters endurance, and legs have 20 times the characters endurance</p>
+  <p>Head is a special case and shares the torso's HP pool.  Damage to the head is considered 2x damage.</p>
+  
+  <h3>4.2 - Damage</h3>
+  <p>Damage is dealt to each section and is kept track of separately.</p>
+  <p>When a player has 0 HP in their legs, they are considered crippled.  Crippled legs make movement cost 3 AP instead of 1 AP.</p>
+  <p>When a player has 0 HP in their arms, they are considered crippled.  Crippled arms make every skill using arms cost 2 more AP.</p>
+  <p>When a player has 0 HP in their torso, they are considered dying.  See the "Dying & Death" section for more details.</p>
+  <p>Damage done to either the arms or legs after they reach 0 HP is transferred to the torso.  So if a player has crippled legs and takes 5 damage to their leg, they instead take 5 damage to the torso.</p>
+
+  <h3>4.3 - Conditions</h3>
+  <p>Conditions</p>
 
 </div>
 `;
