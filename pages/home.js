1e9e442203d2269b436b1838668c848a8cf46340
Alex Irvine <Alex.b.Irvine@gmail.com>
2019-08-07 05:23:08 -0400

    Came up with name!

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,11 @@
 let pageHTML = `
-<h1 class="title-home">Hello World!</h1>
+<h1 class="title-home">DIRE-RPG</h1>
 
 <div class="content-home">
   <p>This is a work in progress for my upcoming RPG system.</p>
 
   <p>Currently, I'm in the process of getting this site functional before I add content.</p>
-  <p>Please check back once content is loaded</p>
+  <p>Soon update history/versions will be added to this page</p>
 </div>
 
 `;
