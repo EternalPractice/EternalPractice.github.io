4792042501bab19e347ea051c85c46c838dd5350
Alex Irvine <alex.b.irvine@gmail.com>
2020-01-17 21:45:48 -0500

    Proving a point

--- data/schoolsOfMagic.js
+++ data/schoolsOfMagic.js
@@ -1,27 +1,55 @@
 let schoolsData = [
   {
-    name: 'Arcane',
-    description: 'a'
+    name: "Arcane",
+    description: "a"
   },
   {
-    name: 'Blood Magic',
-    description: 'b'
+    name: "Blood Magic",
+    description: "b"
   },
   {
-    name: 'Witchery',
-    description: 'b'
+    name: "Witchery",
+    description: "b"
   },
   {
-    name: 'Faith Devotion',
-    description: 'b'
+    name: "Faith Devotion",
+    description: "b"
   },
   {
-    name: 'Pact Magic',
-    description: 'b'
+    name: "Pact Magic",
+    description: "b"
   },
   {
-    name: 'Psionics',
-    description: 'b'
+    name: "Psionics",
+    description: "b"
+  },
+  {
+    name: "One",
+    description: "b"
+  },
+  {
+    name: "TTwo",
+    description: "b"
+  },
+  {
+    name: "Three",
+    description: "b"
+  },
+  {
+    name: "Four",
+    description: "b"
+  },
+  {
+    name: "Five",
+    description: "b"
+  },
+  {
+    name: "Six",
+    description: "b"
+  },
+  {
+    name: "Seven",
+    description: "b"
   }
 ];
 
